7645ad46824a1245352ec2906d455b6b1220f407
GGbeng <15010976823@163.com>
2022-11-28 15:56:50 +0800

    fix: 修复错误限制类型

--- server/src/error/common.error.ts
+++ server/src/error/common.error.ts
@@ -1,8 +1,8 @@
 import { MidwayError } from '@midwayjs/core';
 import { CODE } from '../interface/code';
-
+type codeKey = keyof typeof CODE;
 export class UseError extends MidwayError {
-  constructor(message?: string, codeType?: CODE) {
+  constructor(message?: string, codeType?: codeKey) {
     super(message ?? '未知错误', CODE[codeType ?? 'COMMON_ERROR']);
   }
 }
